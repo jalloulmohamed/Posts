0874bebe336995baec766082eb9e11063702c16f
jalloulmohamed <medjalal1997@gmail.com>
2023-08-13 22:50:40 +0100

    f

--- src/app/compenents/Message.jsx
+++ src/app/compenents/Message.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 export const Message = ({children,avatar,usernmae,description}) => {
   return (
-    <div className='p-4  border-b w-full'>
+    <div className='p-4 my-2  border-b-[#f6f9f9] border-b-[1px] w-full'>
         <div className='flex  items-center '>
             <img className='rounded-full  mr-2' src={avatar} alt=""  width={40}/>
             <h2 className='text-sm  font-bold text-[#181818]'>{usernmae}</h2>
         </div>
-        <div className='ml-12 text-[13px]  py-2 text-[#181818]'>
+        <div className='ml-12  text-[13px]  py-2 text-[#181818]'>
             <p>{description}</p>
         </div>
     </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,13 +10,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className='bg-[#fafafa]' lang="en">
+    <html className='bg-[#f6f9f9]' lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
       </Head>
-      <body className='bg-[#fff] h-screen max-w-[500px] m-auto'>
+      <body className='bg-[#fff] h-screen overflow-auto max-w-[500px] m-auto'>
         <ToastContainer></ToastContainer>
         <Navbar></Navbar>
         {children}
